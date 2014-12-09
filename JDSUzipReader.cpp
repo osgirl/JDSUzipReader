b3a196e2daa9f00f0fabefe5f1b463551abba9e0
ParagonSoftware <paragonsoftwarestudios@gmail.com>
2014-12-09 15:43:45 -0500

    update
    ....

--- JDSUzipReader.cpp
+++ JDSUzipReader.cpp
@@ -4,10 +4,15 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
-
+#include <string>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	getline(cin, argv, '/n');
+	cout << argv << endl;
+
 	system("pause");
 	return 0;
 }
