a71279404c4492230b327daed9cbd3f88865f9a7
ParagonSoftware <paragonsoftwarestudios@gmail.com>
2014-12-30 14:58:37 -0500

    [mike] shipper
    should be done

--- Shipper.h
+++ Shipper.h
@@ -4,17 +4,42 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include "Paragon.h"
 using namespace std;
 
+
+double string_to_double( const std::string& weight )
+{
+   std::istringstream i(weight);
+   double x;
+   if (!(i >> x))
+   {
+	return 0;
+   }
+   return x;
+}
+
 void OpenShipper()
 {
 	// Variable Definitions
-	string itemName, quantity, bin, weight;
-	string DataInFile, substring;
+	string item_Name,
+		shipperQuantity,
+		itemNumber,
+		bin,
+			//MikesIsle,
+			//MikesSection,
+			//MikesHeight,
+		weight,
+		DataInFile;
 	int commaCount = 0;
 	
+	Item MikesItem(item_Name, shipperQuantity, itemNumber, 0.0);
+	//Location MikesLocation(MikesIsle, MikesSection, MikesHeight);
+	Location MikesLocation;
+
+
 	ifstream myFile("shipper.csv");
 
 	if(myFile.is_open())
@@ -26,7 +51,7 @@ void OpenShipper()
 			//Reads the shipper file one line at a time.
 			getline(myFile, DataInFile);
 			// Skips the first line of the shipper file.
-			if(DataInFile == "item,quantity,bin,weight")
+			if(DataInFile == "item,quantity,location,weight")
 			{
 				// for debugging
 				//cout << "in the special condiction\n";
@@ -44,8 +69,11 @@ void OpenShipper()
 					//cout << "comma Count (0) \n";
 					//cout << DataInFile[i];
 					//create name substr
-						itemName = DataInFile[i];
-						cout << itemName;
+						item_Name = DataInFile[i];
+						//debug
+						//cout << itemName;
+						MikesItem.itemName = item_Name;
+						cout << MikesItem.itemName; 
 				}
 
 				if(commaCount == 1 && DataInFile[i] != ',')
@@ -53,8 +81,11 @@ void OpenShipper()
 					//cout << "comma Count (1) \n";
 					//cout << DataInFile[i];
 					//create quantity substr
-						quantity = DataInFile[i];
-						cout << quantity;
+						shipperQuantity = DataInFile[i];
+						//debug
+						//cout << shipperQuantity;
+						MikesItem.quantity = shipperQuantity;
+						cout << MikesItem.quantity;
 				}
 
 				if(commaCount == 2 && DataInFile[i] != ',')
@@ -63,31 +94,35 @@ void OpenShipper()
 					//cout << DataInFile[i];
 					//create bin or location substr
 						bin = DataInFile[i];
-						cout << bin;
+						//cout << bin;
+						Location(bin);
 				}
 
-				if(commaCount == 3 && DataInFile[i] != ',')
+				if(commaCount == 3 && DataInFile[i] != ',' && DataInFile[i] != '\n' )
 				{
 					weight = DataInFile[i];
 					cout << weight;
+					MikesItem.weight = string_to_double( weight );
 				}
 
 				if(DataInFile[i] == ',')
 				{
-					commaCount++;
+					//cout << " /commaCount = " << commaCount << endl;
+					++commaCount;
 					cout << endl;
 					//cout << "commaCount: " << commaCount << endl;
 					//for debugging
 					//cout << "comma position = " << i << endl;
 				}
 
-				else if(commaCount != 0 || commaCount != 1 || commaCount != 2)
+				else if(commaCount < 3)
 				{
 					commaCount = 0;
 				}
+
 			}//end of For Loop
 			std::cout << endl;		
-
+			
 		}//end of while loop
 		
 		/*
@@ -115,37 +150,6 @@ void OpenShipper()
 		system("pause");
 		exit(1);
 	}
-}//End of function
 
-
-
-
-
-	// Old code
-	/*
-	// Input files names into a vector of strings
-	for(int i = 0; i < 13; ++i)
-	{
-		cin >> filename;
-		filenamesList.push_back(filename);
-	}
-
-	// Open the filenames that are stored into
-	// the vector and pass them to the fileParser.
-	for(int i = 0; i < 13; ++i)
-	{
-		filenamesList[i] = filename;
-		ifstream myFile(filename);
-		
-		if(myFile.is_open())
-		{
-			while( getline(myFile, dataInFile, ',') )
-			{
-				fileParser(dataInFile, myFile);
-			}
-		}
-		else
-			cout << "Unable to open " << filenamesList[i];
-	}
-	*/
-
+	myFile.close();
+}//End of function0
